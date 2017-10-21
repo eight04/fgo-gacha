b4e7b958d3982c42bd6b3b8db0132555aafb0b21
eight <eight04@gmail.com>
2017-10-21 18:14:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+這個 REPO 記錄了在 Fate/Grand Order Bilibili 服，伊莉雅 Pickup 卡池的抽卡結果。
+
+抽卡資訊
+-------
+
+* 抽卡時間為 2017-10-20 17:17:34 至 2017-10-21 01:21:57。
+* 該帳號未課金過。
+* 在此之前未使用過任一呼符、聖晶石。
+* 前 260 抽使用呼符，其它使用非課金石。
+
+抽卡結果
+-------
+
+原始資料請查看 data.txt。
+
+總共 264 抽
+
+各星數從者/禮裝所佔比例︰
+★5從者 2.65%
+★5從者(PU) 1.89%
+★4從者 4.17%
+★4從者(PU) 2.65%
+★3從者 43.56%
+★3從者(PU) 0.00%
+★5禮裝 2.65%
+★5禮裝(PU) 0.38%
+★4禮裝 12.12%
+★4禮裝(PU) 3.79%
+★3禮裝 34.85%
+★3禮裝(PU) 3.79%
+
+★4從者︰
+阿斯托爾福 x 1
+童謠 x 4
+安妮．伯妮&瑪莉．瑞德 x 2
+玉藻貓 x 1
+海倫娜．布拉瓦茨基 x 3
+
+★5從者︰
+伊莉雅絲菲爾．馮．愛因茲貝倫 x 5
+阿爾托莉雅．潘德拉貢 x 1
+阿蒂拉 x 1
+
+★3PU禮裝︰
+ZUNGA_ZUNGA x 10
+
+★4PU禮裝︰
+見敵必殺 x 10
+
+★5PU禮裝︰
+蒼玉之魔法少女 x 1
+
+比較有意思的一點，在原始資料中，連續抽到非五星從者的次數，最高正好是 119 次。
+
+圖片
+----
+
+### 抽卡前
+
+Box
+![https://i.imgur.com/FEvtX3l.png](https://i.imgur.com/FEvtX3l.png)
+![https://i.imgur.com/nG5gYIH.png](https://i.imgur.com/nG5gYIH.png)
+![https://i.imgur.com/LW5hbyY.png](https://i.imgur.com/LW5hbyY.png)
+![https://i.imgur.com/icv3ulu.png](https://i.imgur.com/icv3ulu.png)
+
+呼符/聖晶石數量。拍照後又在信箱領了 1 呼符 + 7 石/活動送 1 呼符 + 1 石。
+![https://i.imgur.com/yqwQXwg.png](https://i.imgur.com/yqwQXwg.png)
+
+### 抽卡後
+
+Box
+![https://i.imgur.com/dUQzPZx.png](https://i.imgur.com/dUQzPZx.png)
+
+呼符/聖晶石數量（抽卡後）
+![https://i.imgur.com/F1Q6o6x.png](https://i.imgur.com/F1Q6o6x.png)
---
+++ report.js
@@ -0,0 +1,74 @@
+const _ = {
+	SERVANT: "從者",
+	CE: "禮裝"
+};
+const fs = require("fs");
+const data = JSON.parse(fs.readFileSync("result.json", "utf8"));
+const result = {
+	SERVANT: {},
+	CE: {}
+};
+let total = 0;
+
+for (const r of Object.values(data)) {
+	if (!result[r.type][r.star]) {
+		result[r.type][r.star] = {count: 0, pu: 0};
+	}
+	result[r.type][r.star].count += r.count;
+	if (r.PU) {
+		result[r.type][r.star].pu += r.count;
+	}
+	total += r.count;
+}
+
+console.log(`總共 ${total} 抽\n`)
+
+console.log("各星數從者/禮裝所佔比例︰")
+
+for (const type of Object.keys(result)) {
+	for (const star of [5, 4, 3]) {
+		console.log(`★${star}${_[type]} ${(result[type][star].count * 100 / total).toFixed(2)}%`);
+		console.log(`★${star}${_[type]}(PU) ${(result[type][star].pu * 100 / total).toFixed(2)}%`);
+	}
+}
+
+console.log("");
+
+const STAR_INTERESTED = [4, 5];
+const servants = {};
+
+for (const [name, detail] of Object.entries(data)) {
+	if (detail.type === "SERVANT" && STAR_INTERESTED.includes(detail.star)) {
+		if (!servants[detail.star]) {
+			servants[detail.star] = [];
+		}
+		servants[detail.star].push({name, count: detail.count});
+	}
+}
+
+for (const star of Object.keys(servants)) {
+	console.log(`★${star}${_.SERVANT}︰`);
+	for (const sv of servants[star]) {
+		console.log(`${sv.name} x ${sv.count}`);
+	}
+	console.log("");
+}
+
+const ces = {};
+
+for (const [name, detail] of Object.entries(data)) {
+	if (detail.type === "CE" && detail.PU) {
+		if (!ces[detail.star]) {
+			ces[detail.star] = [];
+		}
+		ces[detail.star].push({name, count: detail.count});
+	}
+}
+
+for (const star of Object.keys(ces)) {
+	console.log(`★${star}PU${_.CE}︰`);
+	for (const ce of ces[star]) {
+		console.log(`${ce.name} x ${ce.count}`);
+	}
+	console.log("");
+}
